3ba42f2e05c12a93605e55fcb12b386dbb1c5a91
ranylm <antche423@gmail.com>
2023-03-07 17:52:40 -0500

    Where is Waldo answered

--- answers.js
+++ answers.js
@@ -82,8 +82,13 @@ and reuse the variable for something else
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
 
-
+whereIsWaldo.splice(1,1);
+whereIsWaldo[1][2] = 'No One';
+console.log(whereIsWaldo[2][1][1]);
 ////////////////////////////////
 //  Excited Kitten
 ////////////////////////////////
