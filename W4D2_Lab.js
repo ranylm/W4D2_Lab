6ba493979a2eaf17c4ddeeeb86a029efeaf7174b
ranylm <antche423@gmail.com>
2023-03-07 13:07:03 -0500

    Second and Final

--- W4D2_Lab.js
+++ W4D2_Lab.js
@@ -5,3 +5,16 @@ function minusOne(number){
 console.log(minusOne(10));
 console.log(minusOne(100));
 console.log(minusOne(Infinity));
+
+function makeSentence(...args){
+  return args.join(' ');
+}
+console.log(makeSentence('I', 'want', 'chimichangas'));
+
+function getLastElement(...args){
+  return args[args.length-1][args[args.length-1].length -1];
+}
+
+console.log(getLastElement([1, 2, 3, 4, 5, 6]));       // 6
+console.log(getLastElement(['a', 'b', 'c']));          // 'c'
+console.log(getLastElement([[1, 2, 3], [4, 5, 6]]));   // [4, 5, 6]
